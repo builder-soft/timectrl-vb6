f11da18d7311f81b91b75f94d1e3fefb13d057c8
Claudio Moscoso <claudio.moscoso@gmail.com>
2016-06-24 15:12:37 -0400

    Se agregan clases para el listener de eventos

--- VB/makeClass.cmd
+++ VB/makeClass.cmd
@@ -1,2 +1,7 @@
 rem java -jar C:\cmoscoso\software\java\com4j\com4j-20110322\tlbimp.jar -o src -p cl.buildersoft.timectrl.api BSzkem.dll
-java -jar Z:\temp\4\20160526\com4j\tlbimp.jar -o src -p cl.buildersoft.timectrl.api BSzkem.dll
+java -jar Z:\temp\4\20160526\com4j\tlbimp.jar -o ..\src -p cl.buildersoft.timectrl.api.com4j BSzkem.dll
+rem java -jar C:\cmoscoso\software\java\com4j\tlbimp.jar -o src -p cl.buildersoft.timectrl.api.com4j BSzkem.dll
+
+
+rem C:\Program Files (x86)\Java\jdk1.6.0_45
+rem       C:\Program Files\Java\jdk1.6.0_45
